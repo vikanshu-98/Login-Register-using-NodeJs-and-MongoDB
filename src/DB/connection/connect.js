f4901c3606da652affaf6d3e55b4c42408a548ca
vchauhan-at-770608018768 <vikanshu.chauhan@novoinvent.com>
2022-05-17 15:24:39 +0530

    deploying changes

--- src/DB/connection/connect.js
+++ src/DB/connection/connect.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const mongo = require('mongoose')
  
-new mongo.connect(process.env.CONNECTION_URL)
+new mongo.connect(process.env.DB_CONNECTION_URL)
 .then(()=>console.log("connect succesfully"))
 .catch((ex)=>console.log(ex));
  
