a190dd0839a798cebf680385c8868fcfaa84281a
vchauhan-at-770608018768 <vikanshu.chauhan@novoinvent.com>
2022-05-17 19:22:46 +0530

    add express-session and modify the partials using passing the variables

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const bcrypt =require('bcrypt')
 const jsonwebtoken = require('jsonwebtoken')
 const cookieParser  =  require("cookie-parser")
 const auth          = require('./Middleware/auth'); 
+const session        = require('express-session');
 const port = process.env.PORT || 3300;
 const statispath = path.join(__dirname, '../public')
 
@@ -18,6 +19,9 @@ app.use(express.static(statispath))
 app.set('views', path.join(__dirname, '../template/views'))
 hbs.registerPartials(path.join(__dirname, '../template/partials'))
 app.use(express.urlencoded({extended:false}))
+
+//use middleware 
+app.use(session({resave:false,saveUninitialized:true,secret: 'keyboard cat', cookie:{ maxAge:12000 }}))
 app.get('/', (req, res) => {
    res.render('index')
 })
@@ -50,7 +54,7 @@ app.post('/', async (req, res) => {
                httpOnly:true
             
             });
-            const result = await fUsers.save(fUsers)
+            const result = await fUsers.save()
             if(result){
                // console.log(result);
                res.redirect('/')
@@ -71,17 +75,30 @@ app.post('/login', async (req, res) => {
    try{ 
       const email       = req.body.email;
       const password    = req.body.password; 
-      const result      = await users.findOne({email:email}); 
-      const resultEmail = await bcrypt.compare(password,result.password);
-      const token  = await result.saveToken()
-      res.cookie('token',token,{
-         expires:new Date(Date.now()+120000),
-         httpOnly:true
-      
-      });
-       
+      const result      = await users.findOne({email:email});
+      const resultEmail = await bcrypt.compare(password,result.password) 
       if(resultEmail){
-         res.render('index') 
+         const token  = await result.saveToken()
+         res.cookie('token',token,{
+            expires:new Date(Date.now()+120000),
+            httpOnly:true
+         
+         });
+         req.session.regenerate(function(err){
+            if(err) next(err); 
+            let session =  req.session
+            session.user = {}
+            session.user.name= result.name;
+            session.user.email= result.email;
+            req.session.save(function(err){
+               if(err) next(err)
+               res.redirect('/')
+            })
+ 
+         })
+         // res.render('index',{
+         //    name:result.name
+         // }) 
       }else{    
          res.send('Invalid Email !!')
       }
@@ -102,13 +119,40 @@ app.get('/jsonWebToken',async (req, res) => {
 //   console.log(result);
 })
 
+const authenticate =  async function(req,res,next){
+  if(req.url=='/logout'){
+      const session       = req.session;
+      req.userData.tokens =[]
+      await req.userData.save() 
+      res.clearCookie('token') 
+      if(session.user){
+         session.user = null
+         session.save(function(err){
+            if(err) next(err)
+            session.regenerate(function(err){
+               if(err) next(err)
+               res.redirect('/')
+            })
+         })
+      }
+  }else{
+   if(req.session.user){
+      req.name= req.session.user.name
+      next()
+   }
+   else{
+      req.name = "Undefined"
+   }
+  }
+}
 
-app.get('/secret', auth, (req, res) => {
- 
-   res.render('secret')
+app.get('/secret', auth,authenticate,async (req, res) => {
+   res.render('secret',{
+     name:req.name 
+   })
  })
 
- app.get('/logout', auth, async(req, res) => {
+ app.get('/logout', auth, authenticate,async(req, res) => {
    try{ 
       // single user logout
 
@@ -118,13 +162,6 @@ app.get('/secret', auth, (req, res) => {
       // });
 
       // multiple user logout 
-      req.userData.tokens=[]
-
-      // console.log("modified data and token is++++++");
-      // console.log(req.userData.length);
-      await req.userData.save() 
-      res.clearCookie('token')
-      res.render('login')
    }catch(err){
       res.status(500).send(err)
    }
